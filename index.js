a131239c7b379c7acc79619e408a150124addfa7
Jasvinder Singh <jvsinghk@gmail.com>
2021-06-01 01:59:46 +0530

    Complete Typemart tasks

--- index.js
+++ index.js
@@ -2,10 +2,31 @@
 exports.__esModule = true;
 var products_1 = require("./products");
 var productName = 'fanny pack';
+var shipping;
+var taxPercent;
+var taxTotal;
+var total;
+var shippingAddress = '789 Broadway, New York City, New York';
 var product = products_1["default"].filter(function (product) {
     return product.name === productName;
 })[0];
 console.log(product);
-if (products_1["default"][0].preOrder === 'true') {
+if (product.preOrder === 'true') {
     console.log('We will send you a message when your product is on its way.');
 }
+if (Number(product.price) > 25) {
+    shipping = 0;
+    console.log('We provide free shipping for this product');
+}
+else {
+    shipping = 5;
+}
+if (shippingAddress.match('New York')) {
+    taxPercent = 0.1;
+}
+else {
+    taxPercent = 0.05;
+}
+taxTotal = Number(product.price) * taxPercent;
+total = Number(product.price) + taxTotal + shipping;
+console.log("\nProduct:  " + product.name + "\nAddress:  " + shippingAddress + "\nPrice:    $" + product.price + "\nTax:      $" + taxTotal.toFixed(2) + "\nShipping: $" + shipping.toFixed(2) + "\nTotal:    $" + total.toFixed(2) + "\n");
--- index.ts
+++ index.ts
@@ -2,11 +2,43 @@ import products from './products';
 
 const productName: string = 'fanny pack';
 
+let shipping: number;
+let taxPercent: number;
+let taxTotal: number;
+let total: number;
+
+const shippingAddress: string = '789 Broadway, New York City, New York';
+
 const product = products.filter(product => 
 product.name === productName)[0];
 
 console.log(product);
 
-if(products[0].preOrder === 'true') {
+if(product.preOrder === 'true') {
     console.log('We will send you a message when your product is on its way.');
 }
+
+if(Number(product.price) > 25) {
+    shipping = 0;
+    console.log('We provide free shipping for this product')
+} else {
+    shipping = 5;
+}
+
+if(shippingAddress.match('New York')) {
+    taxPercent = 0.1;
+} else {
+    taxPercent = 0.05;
+}
+
+taxTotal = Number(product.price) * taxPercent;
+total = Number(product.price) + taxTotal + shipping;
+
+console.log(`
+Product:  ${product.name}
+Address:  ${shippingAddress}
+Price:    $${product.price}
+Tax:      $${taxTotal.toFixed(2)}
+Shipping: $${shipping.toFixed(2)}
+Total:    $${total.toFixed(2)}
+`);
